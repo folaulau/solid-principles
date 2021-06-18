c95e4ba6df301fa614bd6d1317321cce04058b4f
folaulau <folaukaveinga@gmail.com>
2021-06-18 11:12:21 -0600

    added ocp

--- src/main/java/com/lovemesomecoding/solid/user/SpouseService.java
+++ src/main/java/com/lovemesomecoding/solid/user/SpouseService.java
@@ -6,19 +6,15 @@ import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
-public class UserServiceImp implements UserService {
-	
-	/*
-	 * This UserServiceImp focuses on logic that are related to User
-	 */
-	
-	
+public class SpouseService implements UserService {
+
 	@Override
 	public User updateEmail(String newEmail) {
 		/**
 		 * 1. look up if newEmail already exists in db<br>
 		 * 2. change email to newEmail in db<br>
-		 * 3. send email to new email for verification
+		 * 3. send email to new email for verification<br>
+		 * 4. send email to Primary user that spouse email was change
 		 */
 		return null;
 	}
